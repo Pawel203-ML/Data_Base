128db9ad56e6e5c67a436d3f1ca76afd4edc48a0
Pawel Halka <arktykahalka@gmail.com>
2024-11-08 21:24:11 +0100

    Add create_tables() for creating tables with FK

--- bazy_danych.py
+++ bazy_danych.py
@@ -1,7 +1,9 @@
 import sqlite3
 from sqlite3 import Error
 
-#create tables for database
+db_file = 'database.db'
+
+#create connection with database for all functions
 def Create_connection(db_file):
     def decorator(func):
         def wrapper(*args,**kwargs):
@@ -10,3 +12,32 @@ def Create_connection(db_file):
             return result
         return wrapper
     return decorator
+
+#create tables for database 
+@Create_connection(db_file)
+def create_tables(conn):
+    try:
+        cur = conn.cursor()
+
+        cur.execute('''
+                CREATE TABLE IF NOT EXISTS owner(
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    surname TEXT NOT NULL   
+                );
+        ''')
+
+        cur.execute('''
+                CREATE TABLE IF NOT EXISTS vehicles(
+                    id INTEGER PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    fuel TEXT NOT NULL,
+                    capacity TEXT NOT NULL,
+                    FOREIGN KEY (name) REFERENCES owner(id)
+                );
+        ''')
+    except Error as e:
+        print(e)
+
+if __name__ == '__main__':
+    create_tables()
