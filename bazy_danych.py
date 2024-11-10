c2c36068a8b3b46c330bb17265892f0887158cb3
Pawel Halka <arktykahalka@gmail.com>
2024-11-10 13:52:06 +0100

    Add update_user() for user to input data to change and update() for changing data

--- bazy_danych.py
+++ bazy_danych.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+import os
 
 db_file = 'database.db'
 
@@ -73,8 +74,6 @@ def adding_data(conn, table, values):
     cur.execute(sql, values)
     conn.commit()
 
-    
-
 @Create_connection(db_file)
 def select_all(conn, table):
     cur = conn.cursor()
@@ -97,15 +96,85 @@ def select_where(conn, table, **query):
 
     print(rows)
 
+@Create_connection(db_file)
+def update_user(conn):
+    print('--Dane z bazy danych--')
+    cur = conn.cursor()
+    #download names for all tables
+    cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    tables = cur.fetchall()
+    table_names = [table[0] for table in tables]
+    for i in range(len(table_names)):
+        print('')
+        print(f'Tabela: {table_names[i]}')
+        cur.execute(f'''PRAGMA table_info ({table_names[i]})''')
+        columns = [column[1] for column in cur.fetchall() if column[1] != 'id']
+        print('Nazwy kolumn:', end= ' ')
+        for j in range(len(columns)):
+            print(columns[j], end= ' ')
+        print(' \n -- -- --')
+
+    while True:
+        print('Wybierz nazwe tablicy, numer wiersza oraz kolumne z wartoscia do zmiany: ')
+        user_answer = input('tablica, wiersz(id),kolumna=wartosc: ')
+        user = [i.strip() for i in user_answer.split(',')]
+        table = user[0]
+        _id = user[1]
+        #tworzenie slownika do **kwargs w funkcji update()
+        update_data = {}
+        for kv in user[2:]:
+            k, v = kv.split('=')
+            update_data[k.strip()] = v.strip()
+
+        
+        if update(table, _id, **update_data) == True:
+            print('Zamiana dokonana')
+        else:
+            print('Proba nie udana')
+
+        user = input('Czy chcesz kontynuowac? T/N: ')
+        if user.upper() == 'N':
+            break
+        os.system('cls')
+
+
+@Create_connection(db_file)
+def update(conn, table, id, **kwargs):
+    parameters = [f'{k}=?' for k in kwargs.keys()]
+    parameters = ', '.join(parameters)
+
+    values = tuple(v for v in kwargs.values())
+    values += (id,)
+
+    #values = tuple(kwargs.values()) + (id,)
+
+    sql = f'''UPDATE {table} SET {parameters} WHERE id=?'''
+
+    print(parameters, values)
+    try:
+        cur = conn.cursor()
+        cur.execute(sql, values)
+        conn.commit()
+        return True
+    except Error as e:
+        print(e)
+
+
+    
+
+
 if __name__ == '__main__':
     create_tables()
-    adding_data('company', ('Ford', 'Germany'))
+    adding_data('company', ('Ford','Germany'))
     adding_data('company', ('Mitshubishi', 'Japan'))
 
-    #wymaga zglebienia tematu i znalezienia bled ugdyz klucz zewnetrzny nie jest rozpoznawany
     adding_data('vehicles', ('Ford', 'Mustang GT'))
     adding_data('vehicles', ('Mitshubishi','Colt'))
 
     select_all('company')
     select_all('vehicles')
-    #select_where('company', )
+
+    update_user()
+    '''update('company', 2, country='Niemcy')
+    if update('company', 2, country='Niemcy'):
+        print('Udana zamiana')'''
